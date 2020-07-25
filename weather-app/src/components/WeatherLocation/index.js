594c43c7c1045c512e76836317268125f0520bc6
Solórzano <wendy_liz15@hotmail.com>
2020-07-24 23:46:58 -0600

    Agregando promise en el codigo

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -17,19 +17,20 @@ const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 
 const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
-const data ={
+
+ const data ={
     temperature: 5,
     weatherState: SUN,
     humidity: 10,
     wind: '10 m/s',
 }
 
-const data2 ={
-    temperature: 15,
-    weatherState: WINDY,
-    humidity: 20,
-    wind: '10 m/s',
-}
+// const data2 ={
+//     temperature: 15,
+//     weatherState: WINDY,
+//     humidity: 20,
+//     wind: '10 m/s',
+// }
 
 class WeatherLocation extends Component {
     constructor(){
@@ -40,13 +41,44 @@ class WeatherLocation extends Component {
         }
     }
 
+    getWeatherState = weather_data =>{
+        return SUN;
+    }
+
+    //Creando transformaciones de la data.
+    getData = weather_data => {
+        const {humidity, temp} = weather_data.main;
+        const {speed} = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+
+        const data ={
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
+
     handleUpdateClick = () =>{
-        fetch(api_weather);
-        console.log("actualizado");
-        this.setState({
-            city: "Tegucigalpa",
-            data: data2
+        fetch(api_weather).then(resolve =>{
+            return resolve.json();
+        }).then(data => {
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            debugger;
+            this.setState({
+                data:newWeather
+            });             
         });
+
+        // console.log("actualizado");
+
+        // this.setState({
+        //     city: "Tegucigalpa",
+        //     data: data2
+        // });
     }
 
     render(){
